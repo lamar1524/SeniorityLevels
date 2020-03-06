2a7d212d6260fb8478c17d9bce242633a70d17ee
Paweł Młynarski <pawel.wybrzeze@gmail.com>
2020-03-06 07:35:01 +0100

    [P2] Fixed import in tests, removed index.ts from constants directory due to creating @constants alias.

--- src/app/modules/users/components/user/user.component.spec.ts
+++ src/app/modules/users/components/user/user.component.spec.ts
@@ -1,8 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
+import { environment } from 'environments/environment';
 import { initializeApp } from 'firebase';
-import { environment } from 'src/environments/environment';
 import { UserComponent } from './user.component';
 
 describe('UserComponent', () => {
