e24fca8be2131658b087504c2cac6a7f6edc566f
lamar1524 <pawel.mlynarski107@gmail.com>
2020-03-11 10:48:28 +0100

    [P9] Repairs due to using AngularFire api

--- src/app/modules/users/services/users.service.ts
+++ src/app/modules/users/services/users.service.ts
@@ -1,34 +1,33 @@
 import { Injectable } from '@angular/core';
-import { auth as firebaseAuth, database, User } from 'firebase';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { User } from 'firebase';
+import 'firebase/database';
 import { from, Observable } from 'rxjs';
 import { first, map } from 'rxjs/operators';
 
-import { IUser } from '@core/interfaces';
+import { IUser, IUserValues } from '@core/interfaces';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UsersService {
-  private db: firebase.database.Database;
+  constructor(private db: AngularFireDatabase, private firebaseAuth: AngularFireAuth) {}
 
-  constructor() {
-    this.db = database();
-  }
-
-  getCurrentUser = (): User => firebaseAuth().currentUser;
+  getCurrentUser = (): Observable<User> => from(this.firebaseAuth.currentUser).pipe(first());
 
   getUsersList = (): Observable<IUser[]> =>
-    from(this.db.ref('users').once('value')).pipe(
+    from(this.db.database.ref('users').once('value')).pipe(
       first(),
       map((response) => {
         return Object.entries(response.val()).map(
-          (element): IUser => ({ key: element[0], values: JSON.parse(JSON.stringify(element[1])) }),
+          (element): IUser => ({ key: element[0], values: element[1] as IUserValues }),
         );
       }),
     );
 
   getUserByKey(userKey: string): Observable<IUser> {
-    return from(this.db.ref(`users/${userKey}`).once('value')).pipe(
+    return from(this.db.database.ref(`users/${userKey}`).once('value')).pipe(
       first(),
       map((response) => ({
         key: userKey,
