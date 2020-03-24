280051442b6f2d62225b470c90ca6f25af97e97c
lamar1524 <pawel.mlynarski107@gmail.com>
2020-03-24 08:22:38 +0100

    [P-14] Added spinner when loading user's skills. Added message when user didn't fill his skills

--- src/app/modules/users/components/user-profile/user-profile.component.ts
+++ src/app/modules/users/components/user-profile/user-profile.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { throwError } from 'rxjs';
+import { finalize } from 'rxjs/operators';
 
 import { CATEGORIES_AMOUNT } from '@constants/skills.constants';
 import { ICategoryProgress, ISeniorityValues, ISubCategoryValue, IUser } from '@core/interfaces';
@@ -20,6 +21,7 @@ export class UserProfileComponent {
   private userDetails: IUser;
   private categories: ISubCategoryValue[];
   private chosenLevel: seniorityEnum;
+  private levelsLoaded: boolean;
 
   constructor(
     private route: ActivatedRoute,
@@ -27,39 +29,52 @@ export class UserProfileComponent {
     private skillsService: SkillsService,
     private cdRef: ChangeDetectorRef,
   ) {
+    this.levelsLoaded = false;
     this.chosenLevel = seniorityEnum.junior;
     this.categories = [];
     this.userKey = this.route.snapshot.paramMap.get('key');
     this.usersService.getUserByKey(this.userKey).subscribe((details) => {
       this.userDetails = details;
-      this.skillsService.getSkillsData().subscribe(data => {
-        this.computeUsersLevels(data, this.userDetails.key, this.categories); });
+      this.skillsService.getSkillsData().subscribe((data) => {
+        this.computeUsersLevels(data, this.userDetails.key, this.categories);
+      });
     });
     this.imgSrc = 'assets/img/mock/profile_mock.jpg';
   }
 
-  computeUsersLevels(data: ICategoryProgress[], userId: string, arrayToPut: any[]) {
-    data.forEach((element: any) => {
-      this.skillsService.getValuesBySkillNames(userId, element.title).subscribe(
-        (values) => {
-          if (values !== null) {
-            const categoryValues: ISeniorityValues[] = Object.values(values);
-            arrayToPut.push({
-              title: element.title,
-              levels: this.skillsService.getProgressOf(categoryValues, CATEGORIES_AMOUNT[element.title]),
-            });
-          }
-          this.cdRef.markForCheck();
-        },
-        (error) => {
-          throwError(error);
-        },
-      );
+  computeUsersLevels(data: ICategoryProgress[], userId: string, arrayToPut: ISubCategoryValue[]) {
+    data.forEach((element: ICategoryProgress) => {
+      this.skillsService
+        .getValuesBySkillNames(userId, element.title)
+        .pipe(
+          finalize(() => {
+            this.levelsLoaded = true;
+          }),
+        )
+        .subscribe(
+          (values) => {
+            if (values !== null) {
+              const categoryValues: ISeniorityValues[] = Object.values(values);
+              arrayToPut.push({
+                title: element.title,
+                levels: this.skillsService.getProgressOf(categoryValues, CATEGORIES_AMOUNT[element.title]),
+              });
+            }
+            this.cdRef.markForCheck();
+          },
+          (error) => {
+            throwError(error);
+          },
+        );
     });
   }
 
   chooseLevel(level: seniorityEnum) {
     this.chosenLevel = level;
     this.cdRef.markForCheck();
+  }
+
+  get levelsFound() {
+    return this.levelsLoaded && this.categories.length > 0;
   }
 }
