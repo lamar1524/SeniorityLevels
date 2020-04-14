63b3b9d9f12031c9ed7914b7d634a5ae17e17fa9
lamar1524 <pawel.mlynarski107@gmail.com>
2020-04-14 11:25:56 +0200

    [P-6] Added tests for data sharing service

--- src/app/shared/services/data-sharing.service.spec.ts
+++ src/app/shared/services/data-sharing.service.spec.ts
@@ -1,12 +1,50 @@
 import { TestBed } from '@angular/core/testing';
+import { Observable } from 'rxjs';
 
+import { themeEnum } from '../enum/theme.enum';
 import { DataSharingService } from './data-sharing.service';
 
 describe('DataSharingService', () => {
+  let service: DataSharingService;
+  const themeKey = 'Theme';
+
   beforeEach(() => TestBed.configureTestingModule({}));
 
+  beforeEach(() => {
+    service = TestBed.inject(DataSharingService);
+  });
+
   it('should be created', () => {
-    const service: DataSharingService = TestBed.inject(DataSharingService);
     expect(service).toBeTruthy();
+  });
+
+  describe('getTheme method', () => {
+    it('should return observable', () => {
+      expect(service.getTheme() instanceof Observable).toBeTruthy();
+    });
+  });
+
+  describe('setTheme method', () => {
+    it('should call setThemeInStorage method', () => {
+      spyOn(service, 'setThemeInStorage');
+      service.setTheme(themeEnum.light);
+      expect(service.setThemeInStorage).toHaveBeenCalledWith(themeEnum.light);
+    });
+  });
+
+  describe('getThemeFromStorage method', () => {
+    it('should call getItem method', () => {
+      spyOn(localStorage, 'getItem');
+      service.getThemeFromStorage();
+      expect(localStorage.getItem).toHaveBeenCalledWith(themeKey);
+    });
+  });
+
+  describe('toggleTheme method', () => {
+    it('should call setTheme method', () => {
+      spyOn(service, 'setTheme');
+      service.toggleTheme();
+      expect(service.setTheme).toHaveBeenCalled();
+    });
   });
 });
