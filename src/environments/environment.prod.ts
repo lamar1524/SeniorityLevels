5aeb3d79f51c338a49f7e52f1ad6aaca5a3acf53
lamar1524 <pawel.wybrzeze@gmail.com>
2020-05-17 16:07:28 +0200

    removed firebase connection credentials, changed them due to security

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,3 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: 'AIzaSyAgUlluVa_qttrAwd7iKqVaa5_6u3xJLE0',
-    authDomain: 'senioritylevels-82ee8.firebaseapp.com',
-    databaseURL: 'https://senioritylevels-82ee8.firebaseio.com',
-    projectId: 'senioritylevels-82ee8',
-    storageBucket: 'senioritylevels-82ee8.appspot.com',
-    messagingSenderId: '295870760368',
-    appId: '1:295870760368:web:929e2be91e1afbad6e4275',
-  },
 };
